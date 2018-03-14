d02bec3c50da9614eeee3f84329b52724fef4766
akanekotyui <a_roid_ba_sky@yahoo.co.jp>
2018-03-14 19:32:17 +0900

    week3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,33 @@
 class BlogsController < ApplicationController
 	def index
+		@blogs = Blog.all
 	end
+
+	def new
+		#コントローラがblogsという名前なのでモデル名はblogになる
+		@blog = Blog.new
+		#@をつけるとビューに送ることができる
+		#ブログモデルが入る空の箱（インスタンス）
+	end
+
+	def create
+		blog = Blog.new(blog_params)
+		#@なしだと送ることができない
+		blog.save
+		#create.html.erbはないのでリダイレクトする
+		redirect_to '/blogs'
+	end
+
+	def show
+		@blog = Blog.find(params[:id])
+	end
+
+	private #スコープがかわる
+	#ビューからのデータはparamsというメソッドの中にはいっている。
+	#ストロングパラメータはセキュリティを強固にしている（いるものだけをpermitしている)
+	#rails4以降でblog_paramsができた。
+		def blog_params
+	        params.require(:blog).permit(:title, :body, :category)
+	    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get 'blogs/create'
+
+  get 'blogs/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/blogs' => 'blogs#index'
+  get '/blogs/new' => 'blogs#new'
+
+  #form_forの行き先を定義
+  post '/blogs' => 'blogs#create'
+  post '/blogs' =>'blogs#index'
+
+  get '/blogs/:id' => 'blogs#show', as: 'blog'
 end
